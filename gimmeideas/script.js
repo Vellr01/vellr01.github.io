ea371e6222eebf57e1f82366dae23642967742cc
Wolfuri <darkwolf.redwolf@gmail.com>
2026-02-27 20:39:43 +0100

    Made something.. I think ?

---
+++ gimmeideas/script.js
@@ -0,0 +1,13 @@
+
+submit = () => {
+    let theIdeaThisNiceUserJustSent = document.getElementById("daInput").value
+    window.location = `https://wolfuri.pythonanywhere.com/${theIdeaThisNiceUserJustSent}`;
+};
+
+document.getElementById("daButton").addEventListener("click", () => {
+    submit();
+});
+
+document.getElementById("daInput").addEventListener("keypress", e => {
+    if (e.key === "Enter") submit();
+})
